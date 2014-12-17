675308a9e0933b2bd70c92d960a8e000b4df1628
16sixteen <929845256@qq.com>
2014-12-17 11:06:46 +0800

    。。。

--- test.py
+++ test.py
@@ -1,57 +1,20 @@
-#add new thing
-#i have add new thing
+class A(object):
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+		print "A"
 
-#change third time
+class B(A):
+	def __init__(self, username, password):
+		print "B"
+		A.__init__(self, username, password)
 
-import tornado.httpserver
-import tornado.ioloop
-import tornado.web
-import tornado.options
-#i have delete something
-
-# from tornado.options import define, options
-# define("port", default=8000, help="run on the given port", type=int)
-
-class BaseHandler(tornado.web.RequestHandler):
-    def get_current_user(self):
-        return self.get_secure_cookie("username")
-
-class LoginHandler(BaseHandler):
-    def get(self):
-        self.render('login.html')
-
-    def post(self):
-        self.set_secure_cookie("username", self.get_argument("username"))
-        self.redirect("/")
-
-class WelcomeHandler(BaseHandler):
-    @tornado.web.authenticated
-    def get(self):
-        self.render('index.html', user=self.current_user)
-
-class LogoutHandler(BaseHandler):
-    def get(self):
-        if (self.get_argument("logout", None)):
-            self.clear_cookie("username")
-            self.redirect("/")
+class C(A):
+	def __init__(self, username, password):
+		print "C"
+		A.__init__(self, username, password)
 
 if __name__ == "__main__":
-    tornado.options.parse_command_line()
-
-    settings = {
-        "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-        "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
-        "xsrf_cookies": True,
-        "login_url": "/login"
-    }
-
-    application = tornado.web.Application([
-        (r'/', WelcomeHandler),
-        (r'/login', LoginHandler),
-        (r'/logout', LogoutHandler)
-    ], **settings)
-
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
-    
+	c = C("langge", "jiahao")
+	print c.username, c.password
+	print c
